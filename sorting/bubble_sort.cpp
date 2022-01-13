0ff04142965765148e64352fe72ec5613813b2b7
Louis2602 <trantunglam135790@gmail.com>
2022-01-13 09:12:25 +0700

    add new files

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {3, 2, 5, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n - 1; ++i)
+        for (int j = i + 1; j < n; ++j)
+            if (arr[i] > arr[j])
+                swap(arr[i], arr[j]);
+
+    // output
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    return 0;
+}
