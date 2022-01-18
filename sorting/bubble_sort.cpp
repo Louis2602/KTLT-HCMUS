62b2e61384feee36f22da579a9a36891c5298dbc
Louis2602 <trantunglam135790@gmail.com>
2022-01-18 13:51:34 +0700

    updates

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -5,6 +5,7 @@ int main()
 {
     int arr[] = {3, 2, 5, 1, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
+
     for (int i = 0; i < n - 1; ++i)
         for (int j = i + 1; j < n; ++j)
             if (arr[i] > arr[j])
---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {3, 2, 5, 1, 6, 4, 9, 7, 11};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    int minIndex, minValue;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        minIndex = i;
+        minValue = a[i];
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (a[j] < minValue)
+            {
+                minIndex = j;
+                minValue = a[j];
+            }
+        }
+        a[minIndex] = a[i];
+        a[i] = minValue;
+    }
+
+    // output
+    for (int i = 0; i < n; ++i)
+        cout << a[i] << " ";
+    return 0;
+}
