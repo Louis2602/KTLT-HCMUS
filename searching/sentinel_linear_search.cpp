47ec6f83db3aacff9f708f74973a7b08f34c6636
Louis2602 <trantunglam135790@gmail.com>
2022-01-19 21:54:54 +0700

    update

--- searching/sentinel_linear_search.cpp
+++ searching/sentinel_linear_search.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool findKey(int a[], int k, int n)
+bool search(int a[], int k, int n)
 {
     a[n] = k;
     int i = 0;
@@ -15,7 +15,7 @@ int main()
     int a[] = {10, 3, 2, 6, 8, 12, 15};
     int n = sizeof(a) / sizeof(a[0]);
     int k = 7;
-    if (findKey(a, k, n))
+    if (search(a, k, n))
         cout << "k is in the array";
     else
         cout << "k is not in the array";
