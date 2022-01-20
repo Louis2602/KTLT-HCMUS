2ce2674a7c96305ad6bfa54acda31605891c6ee0
Louis2602 <trantunglam135790@gmail.com>
2022-01-20 10:00:52 +0700

    update

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 void insertionSort(int arr[], int n)
 {
-    int i, key, j;
+    int j;
     // neu co 1 phan tu thi khong can sap xep, vi vay ta chay i tu 1
-    for (i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        key = arr[i];
+        int key = arr[i];
         j = i - 1;
         while (j >= 0 && arr[j] > key)
         {
@@ -18,7 +18,12 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
-
+void insertionSort_sentinel(int arr[], int n)
+{
+}
+void insertionSort_binary_search(int arr[], int n)
+{
+}
 void printArray(int arr[], int n)
 {
     int i;
@@ -32,7 +37,8 @@ int main()
     int arr[] = {12, 11, 13, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    insertionSort(arr, n);
+    // insertionSort(arr, n);
+    insertionSort_sentinel(arr, n);
     printArray(arr, n);
 
     return 0;
