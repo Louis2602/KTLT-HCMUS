68d950c4aeca6ac0a828b256132e990ffe82cfc5
Louis2602 <trantunglam135790@gmail.com>
2022-01-20 08:03:07 +0700

    update

--- sorting/bubble_sort_1.cpp
+++ sorting/bubble_sort_1.cpp
@@ -6,7 +6,7 @@ int main()
     int a[] = {11, 8, 5, 2, 7, 1};
     int n = sizeof(a) / sizeof(a[0]);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
         for (int j = n - 1; j >= i; j--)
             if (a[j] < a[j - 1])
                 swap(a[j], a[j - 1]);
