dcf35fb2df1c1dc2151bab0289b58301e9ff0f86
Louis2602 <trantunglam135790@gmail.com>
2022-02-21 09:37:04 +0700

    add new files

---
+++ Lab/Lab2/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+struct Players
+{
+    string role;
+    string name;
+    int year;
+    string country;
+    string club;
+};
+void ReadFile(char *input, Players players[])
+{
+    ifstream in;
+    in.open(input);
+
+    int i = 0;
+    while (!in.eof())
+    {
+        getline(in, players[i].role, '/');
+        getline(in, players[i].name, '/');
+        in >> players[i].year;
+        in.get(); // skip / after yeas
+        getline(in, players[i].country, '/');
+        getline(in, players[i].club, '\n');
+        i++;
+    }
+    in.close();
+}
+void findPlayersBornIn1994(char *input, Players players[])
+{
+    ReadFile(input, players);
+    cout << "All players were born in 1994:\n";
+    for (int i = 0; i < 81; i++)
+    {
+        if (players[i].year == 1994)
+            cout << players[i].name << '\n';
+    }
+}
+
+void findManchesterUnitedPlayers(char *input, Players players[])
+{
+    ReadFile(input, players);
+    cout << "All players that played for Manchester United:\n";
+    for (int i = 0; i < 81; i++)
+    {
+        if (players[i].club == "Manchester United")
+            cout << players[i].name << '\n';
+    }
+}
+void findAllFW_ManchesterCity(char *input, Players players[])
+{
+    ReadFile(input, players);
+    cout << "All FW players that played for Manchester City:\n";
+    for (int i = 0; i < 81; i++)
+    {
+        if (players[i].role == "FW" && players[i].club == "Manchester City")
+            cout << players[i].name << '\n';
+    }
+}
+void swap(string &a, string &b)
+{
+    string tmp = a;
+    a = b;
+    b = tmp;
+}
+void sortPlayers(char *input, Players players[])
+{
+    ReadFile(input, players);
+    string lastName[81];
+    for (int i = 0; i < 81; i++)
+    {
+        for (int j = 0; j < players[i].name.size(); j++)
+        {
+            if (players[i].name[j] == ' ')
+            {
+                lastName[i] = players[i].name.substr(j, players[i].name.size() - 1);
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < 80; i++)
+        for (int j = i + 1; j < 81; j++)
+            if (players[j].year >= players[i].year)
+                swap(players[i], players[j]);
+
+    for (int i = 0; i < 80; i++)
+        for (int j = i + 1; j < 81; j++)
+            if (players[j].year == players[i].year)
+                if (lastName[j] < lastName[i])
+                    swap(players[i], players[j]);
+    for (int i = 0; i < 81; i++)
+        cout << i + 1 << "/" << players[i].name << "/" << players[i].year << '\n';
+}
+int main()
+{
+    Players players[81];
+    char input[] = "Players.txt";
+    findPlayersBornIn1994(input, players);
+    // findManchesterUnitedPlayers(input, players);
+    // findAllFW_ManchesterCity(input, players);
+    // sortPlayers(input, players);
+    return 0;
+}
