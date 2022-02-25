149e5623166ed0f6d3d03a52dde7c82be2b35eaf
Louis2602 <trantunglam135790@gmail.com>
2022-02-25 09:20:22 +0700

    add interpolation_search and shaker_sort

---
+++ sorting/shaker_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void shakerSort(int a[], int n)
+{
+    int l, r, mark;
+    l = 0;
+    r = mark = n - 1;
+    while (l < r)
+    {
+        for (int j = l; j < r; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+                mark = j;
+            }
+        }
+        r = mark;
+        for (int i = r; i > l; i--)
+        {
+            if (a[i] < a[i - 1])
+            {
+                int tmp = a[i];
+                a[i] = a[i - 1];
+                a[i - 1] = tmp;
+                mark = i;
+            }
+        }
+        l = mark;
+    }
+}
+void print(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+}
+int main()
+{
+    int a[] = {9, 8, 7, 5, 3, 6, 1};
+    int n = 7;
+    shakerSort(a, n);
+    print(a, n);
+    return 0;
+}
