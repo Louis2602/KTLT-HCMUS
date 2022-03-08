d677d88524a013f64ee9bf363909e17f136a6e3c
Louis2602 <trantunglam135790@gmail.com>
2022-03-08 23:48:24 +0700

    update

--- Lab/Lab1/Array-String/2D-Array/multiple.cpp
+++ Lab/Lab1/Array-String/2D-Array/multiple.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 void input(int a[][100], int r, int c)
 {
     for (int i = 0; i < r; i++)
     {
         for (int j = 0; j < c; j++)
-            cin >> a[i][j];
+            a[i][j] = rand() % 10;
     }
 }
 void multiple2DArray(int a[][100], int b[][100], int res[][100], int m, int n, int q)
 {
-    if (m == q)
-    {
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < q; j++)
-                for (int k = 0; k < n; k++)
-                    res[i][j] += (a[i][k] * b[k][j]);
-    }
-    else
-    {
-        cout << "Can't do multiplication on these matrix" << '\n';
-    }
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < q; j++)
+            for (int k = 0; k < n; k++)
+                res[i][j] += (a[i][k] * b[k][j]);
 }
 void print(int res[][100], int m, int n)
 {
@@ -33,18 +27,27 @@ void print(int res[][100], int m, int n)
 }
 int main()
 {
-    int m, n, p, q;
+    srand(time(0));
+    int ma, na, mb, nb;
     int a[100][100], b[100][100], res[100][100];
     cout << "Rows and Cols for matrix A: ";
-    cin >> m >> n;
+    cin >> ma >> na;
     cout << "Rows and Cols for matrix B: ";
-    cin >> p >> q;
+    cin >> mb >> nb;
     cout << "Matrix A:\n";
-    input(a, m, n);
+    input(a, ma, na);
+    print(a, ma, na);
     cout << "Matrix B:\n";
-    input(b, p, q);
-    cout << "Product of 2 matrix A and B:\n";
-    multiple2DArray(a, b, res, m, n, q);
-    print(res, m, q);
+    input(b, mb, nb);
+    print(b, mb, nb);
+    if (na == mb)
+    {
+        cout << "Product of 2 matrix A and B:\n";
+        multiple2DArray(a, b, res, ma, na, nb);
+        print(res, ma, nb);
+    }
+    else
+        cout << "Error! Matrix cannot be multiplied";
+
     return 0;
 }
