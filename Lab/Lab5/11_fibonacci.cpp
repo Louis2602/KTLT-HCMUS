8b554f6ce937bb28ac245c672efd86a87a2e5f8f
Louis2602 <trantunglam135790@gmail.com>
2022-03-15 11:11:53 +0700

    update

--- Lab/Lab5/11_fibonacci.cpp
+++ Lab/Lab5/11_fibonacci.cpp
@@ -3,16 +3,31 @@ using namespace std;
 
 int fibonacci(int n)
 {
-    if (n == 1)
-        return 1;
-    if (n == 0)
-        return 0;
+    if (n <= 1)
+        return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
+}
+int improvedFibonacci(int n, int *cache)
+{
+    if (cache[n])
+        return cache[n];
+    cache[n] = improvedFibonacci(n - 1, cache) + improvedFibonacci(n - 2, cache);
+    return cache[n];
 }
 int main()
 {
     int n;
     cin >> n;
-    cout << "Fibonacci at " << n << "th = " << fibonacci(n);
+    // cout << "Fibonacci at " << n << "th = " << fibonacci(n);
+    // long long f0 = 0, f1 = 1, f;
+    // for (int i = 2; i <= n; i++)
+    // {
+    //     f = f0 + f1;
+    //     f0 = f1;
+    //     f1 = f;
+    // }
+    // cout << f;
+    int cache[3] = {0, 1, 1};
+    cout << "Fibonacci at " << n << "th = " << improvedFibonacci(n, cache);
     return 0;
 }
