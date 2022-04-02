281b5db68c5f6e61fdb05e30c5892971a5a20c7a
Louis2602 <trantunglam135790@gmail.com>
2022-04-03 00:51:06 +0700

    add find max sum submatrix

---
+++ Lab/Lab3/findMaxSumSubMatrix.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int **generateMatrix(int **a, int &width, int &length)
+{
+    srand(time(NULL));
+    a = new int *[width];
+    for (int i = 0; i < width; i++)
+    {
+        a[i] = new int[length];
+        for (int j = 0; j < length; j++)
+            a[i][j] = rand() % 10 - 5;
+    }
+    return a;
+}
+void print(int **a, int width, int length)
+{
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < length; j++)
+            cout << a[i][j] << " ";
+        cout << '\n';
+    }
+}
+void findMaxSumSubmatrix(int **a, int width, int length, int width_, int length_)
+{
+    int maxSum = 0, currSum = 0;
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            currSum = 0;
+            for (int m = i; m < width_; m++)
+            {
+                for (int n = j; n < length_; n++)
+                {
+                    currSum += a[m][n];
+                }
+            }
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    cout << "Max sum submatrix is: " << maxSum;
+}
+int main()
+{
+    int **a;
+    int width, length;
+    cout << "Enter matrix width: ";
+    cin >> width;
+    cout << "Enter matrix length: ";
+    cin >> length;
+    int **matrix;
+    matrix = generateMatrix(a, width, length);
+    print(matrix, width, length);
+    int width_, length_;
+    cout << "Enter submatrix width: ";
+    cin >> width_;
+    cout << "Enter submatrix length: ";
+    cin >> length_;
+    findMaxSumSubmatrix(matrix, width, length, width_, length_);
+    return 0;
+}
